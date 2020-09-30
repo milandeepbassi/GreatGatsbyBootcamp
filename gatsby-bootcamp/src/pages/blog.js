95bf5d0c0c9c65d5141764eb690fc082f4f915a4
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-01 00:57:09 +0100

    Added new blog page starter.

---
+++ gatsby-bootcamp/src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Milan's Great Gatsby Bootcamp Site</h1>
+      <h2>This is the blog page.</h2>
+    </div>
+  )
+}
+
+export default BlogPage
