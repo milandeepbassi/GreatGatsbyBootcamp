f69766bebab0df4eac8cf4fdcdb31ccaa422f4d0
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-01 01:04:32 +0100

    Added links between pages.

--- gatsby-bootcamp/src/pages/blog.js
+++ gatsby-bootcamp/src/pages/blog.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const BlogPage = () => {
   return (
     <div>
       <h1>Milan's Great Gatsby Bootcamp Site</h1>
       <h2>This is the blog page.</h2>
+      <p><Link to="/">Home Page</Link></p>
+      <p><Link to="/blog">Blog Page</Link></p>
+      <p><Link to="/about">About Page</Link></p>
+      <p><Link to="/contact">Contact Page</Link></p>
     </div>
   )
 }
