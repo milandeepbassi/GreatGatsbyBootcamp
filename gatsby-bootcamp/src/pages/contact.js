29f12d0363d453ae70f84801610af703e4ae7c3b
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-01 01:07:25 +0100

    Added external link.

--- gatsby-bootcamp/src/pages/contact.js
+++ gatsby-bootcamp/src/pages/contact.js
@@ -6,6 +6,7 @@ const ContactPage = () => {
     <div>
       <h1>Milan's Great Gatsby Bootcamp Site</h1>
       <h2>This is the contact page.</h2>
+      <p><a href="https://www.twitter.com/milanbassi" target="_blank">Connect on Twitter</a></p>
       <p><Link to="/">Home Page</Link></p>
       <p><Link to="/blog">Blog Page</Link></p>
       <p><Link to="/about">About Page</Link></p>
