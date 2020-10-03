32541172f4fa5ced95097b30d7d29085c648fbf9
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-03 15:27:38 +0100

    Dynamically create pages.

--- gatsby-bootcamp/gatsby-node.js
+++ gatsby-bootcamp/gatsby-node.js
@@ -12,3 +12,32 @@ module.exports.onCreateNode = ({ node, actions }) => {
         })
     }    
   }
+
+  module.exports.createPages = async ({graphql, actions}) => {
+        const { createPage } = actions
+        const blogTemplate = path.resolve('./src/templates/blog.js') 
+        const res = await graphql(`
+            query{
+                allMarkdownRemark{
+                    edges{
+                        node{
+                            id
+                            fields {
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `)
+        res.data.allMarkdownRemark.edges.forEach((edge) => {
+            createPage({
+                component: blogTemplate,
+                path: `/blog/${edge.node.fields.slug}`,
+                context: {
+                    id: edge.node.id,
+                    slug: edge.node.fields.slug
+                }
+            })
+        })
+    }
---
+++ gatsby-bootcamp/src/templates/blog.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <Layout>
+            Blog Template
+        </Layout>        
+    )
+}
+
+export default Blog
