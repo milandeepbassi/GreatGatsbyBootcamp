ade9a430c80b3e355968279278c0642fd1d62cd6
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-03 16:52:54 +0100

    Setup contetnful CMS.

--- gatsby-bootcamp/gatsby-config.js
+++ gatsby-bootcamp/gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     }
   ]
 }
