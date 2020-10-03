5fdd18f4715067ab0acc1464fdcc88b57b922659
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-03 01:54:53 +0100

    Added SASS and basic style to project.

--- gatsby-bootcamp/src/components/layout.js
+++ gatsby-bootcamp/src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import '../styles/index.scss'
 
 const DefaultLayout = (props) => {
     return (
