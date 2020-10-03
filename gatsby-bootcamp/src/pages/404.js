7877bccd6defb20f4f292b5d5f0c760460725f9a
Milandeep Bassi <milandeepbassi@gmail.com>
2020-10-03 18:11:08 +0100

    Added 404 page.

---
+++ gatsby-bootcamp/src/pages/404.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h2>This page is not found.</h2>
+    </Layout>
+  )
+}
+
+export default NotFound
